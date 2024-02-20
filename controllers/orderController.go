e3a58580d176bb5ddcf4f3ba7fbe715ef2469760
codepnw <idchokms01@outlook.com>
2024-02-20 23:35:19 +0700

    orderItem: model, controller

--- controllers/orderController.go
+++ controllers/orderController.go
@@ -1,8 +1,27 @@
 package controllers
 
 import (
+	"context"
+	"time"
+
 	"github.com/codepnw/restaurant-api/database"
+	"github.com/codepnw/restaurant-api/models"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
+var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
 var orderCollection *mongo.Collection = database.OpenCollection(database.Client, "order")
+
+func OrderItemOrderCreator(order models.Order) string {
+	order.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+	order.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+	order.ID = primitive.NewObjectID()
+	order.Order_id = order.ID.Hex()
+
+	orderCollection.InsertOne(ctx, order)
+	defer cancel()
+
+	return order.Order_id
+}
